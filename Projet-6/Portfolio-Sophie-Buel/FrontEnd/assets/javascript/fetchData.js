5a02403a1c0f2342d484cabda6cca37ca00addfa
Voxalium <voxalium@gmail.com>
2023-10-13 18:08:25 +0200

    p6 - Filter Data

--- Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/fetchData.js
+++
@@ -1,7 +0,0 @@
-export async function fetchData(url) {
-    const res = await fetch(url);
-    if (!res.ok) {
-        throw new Error("Can't fetch data");
-    }
-    return res.json();
-}
--- Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/index.js
+++ Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/index.js
@@ -1,18 +1,91 @@
-import { fetchData } from "./fetchData.js";
-const url = "http://localhost:5678/api/works";
 const gallery = document.body.querySelector(".gallery");
-fetchData(url).then((data) => {
-    //Boucle titre
+const tous = document.querySelector(".filter-tous");
+const objets = document.querySelector(".filter-objets");
+const appart = document.querySelector(".filter-appart");
+const hotel = document.querySelector(".filter-hotels");
+const button = document.getElementsByClassName("button");
+
+//Button selection
+const addSelectClass = function () {
+    removeSelectClass();
+    this.classList.add("button-selected");
+};
+const removeSelectClass = function () {
+    for (let i = 0; i < button.length; i++) {
+        button[i].classList.remove("button-selected");
+    }
+};
+
+for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", addSelectClass);
+}
+
+// Fetch DATA
+
+async function fetchData() {
+    const res = await fetch("http://localhost:5678/api/works");
+    if (!res.ok) {
+        throw new Error("Can't fetch data");
+    }
+    return res.json();
+}
+
+//Filter
+
+tous.addEventListener("click", () => {
+    fetchData().then((data) => {
+        let f = data;
+        drawData(f);
+    });
+});
+
+objets.addEventListener("click", () => {
+    fetchData().then((data) => {
+        let f = data.filter((item) => {
+            return item.category.name === "Objets";
+        });
+        drawData(f);
+    });
+});
+
+appart.addEventListener("click", () => {
+    fetchData().then((data) => {
+        let f = data.filter((item) => {
+            return item.category.name === "Appartements";
+        });
+        drawData(f);
+    });
+});
+
+hotel.addEventListener("click", () => {
+    fetchData().then((data) => {
+        let f = data.filter((item) => {
+            return item.category.name === "Hotels & restaurants";
+        });
+        drawData(f);
+    });
+});
+
+//Default data
+fetchData().then((data) => {
+    let f = data;
+    drawData(f);
+});
+
+//Draw data
+const drawData = function (data) {
+    while (gallery.hasChildNodes()) {
+        gallery.removeChild(gallery.firstChild);
+    }
     for (let i in data) {
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
         img.src = `${data[i].imageUrl}`;
         figcaption.innerHTML = `${data[i].title}`;
+
         figure.appendChild(img);
         figure.appendChild(figcaption);
-
         gallery.appendChild(figure);
-        //console.log(`${i}: ${data[i].title}`);
     }
-});
+};
