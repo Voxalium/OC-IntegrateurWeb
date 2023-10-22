e9e3ad15e03c1f60e9f16855aed8f76ed36bc144
Voxalium <voxalium@gmail.com>
2023-10-22 14:39:03 +0200

    p6 Modal ajout-photo

--- Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/index-edit.js
+++ Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/index-edit.js
@@ -2,10 +2,8 @@ import { drawData } from "./drawData.js";
 import data from "./fetchData.js";
 //DOM
 const gallery = document.querySelector(".gallery");
-const modalGrid = document.querySelector('.modal-grid')
 //Fetch
 const dataSet = new Set();
 dataSet.add(data);
 //Draw data
 drawData(dataSet, gallery);
-drawData(dataSet, modalGrid);
--- Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/modal.js
+++ Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/modal.js
@@ -1,4 +1,4 @@
-import { drawData} from "./drawData.js";
+import { drawData } from "./drawData.js";
 import data from "./fetchData.js";
 
 const dataSet = new Set();
@@ -7,14 +7,61 @@ dataSet.add(data);
 const modal = document.querySelector(".modal");
 const edit = document.querySelector(".edit-projects");
 const xmark = document.querySelector(".xmark");
-const modalGrid = document.querySelector(".modal-grid");
+const container = document.querySelector(".container");
+const button = document.querySelector(".button-modal");
+const title = document.querySelector(".title-modal");
 
+const removeChild = (container) => {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+};
+const createGallery = () => {
+    removeChild(container);
+    drawData(dataSet, container);
+    container.className = "container gallery-modal";
+    title.innerHTML = "Galerie photo";
+    button.innerHTML = "Ajouter une photo";
+};
+
+const createAddPhoto = () => {
+    removeChild(container);
+    container.className = "container add-photo-modal";
+    title.innerHTML = "Ajout photo";
+    button.innerHTML = "Valider";
+
+    const imgContainer = document.createElement("div");
+    imgContainer.className = "img-container";
+    const titleLabel = document.createElement("label");
+    titleLabel.innerHTML = "Titre";
+    const categoryLabel = document.createElement("label");
+    categoryLabel.innerHTML = "Categorie";
+    const titleInput = document.createElement("input");
+    const categorySelect = document.createElement("select");
+    const options = ["Objets", "Appartements", "Hôtels et restaurants"];
+
+    container.appendChild(imgContainer);
+    container.appendChild(titleLabel);
+    container.appendChild(titleInput);
+    container.appendChild(categoryLabel);
+
+    for (let i in options) {
+        const opt = document.createElement("option");
+        opt.value = options[i];
+        opt.text = options[i];
+        categorySelect.add(opt, null);
+    }
+    container.appendChild(categorySelect);
+};
 xmark.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
 edit.addEventListener("click", () => {
+    createGallery();
     modal.style.display = "flex";
 });
 
-drawData(dataSet, modalGrid);
+button.addEventListener("click", () => {
+    createAddPhoto();
+});
