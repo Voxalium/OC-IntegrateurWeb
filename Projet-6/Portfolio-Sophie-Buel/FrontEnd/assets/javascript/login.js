c0b535d4e19dcaa5f0568f5e7f2ca12a8adf7705
Voxalium <voxalium@gmail.com>
2023-10-26 16:12:15 +0200

    p6 - fonctionnal add photo

--- Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/login.js
+++ Projet-6/Portfolio-Sophie-Buel/FrontEnd/assets/javascript/login.js
@@ -1,46 +1,4 @@
-const loginUrl = "http://localhost:5678/api/users/login";
+import { handleFormSubmit } from "./request.js";
 const loginForm = document.getElementById("loginForm");
-
-function handleFormSubmit(e) {
-    e.preventDefault();
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
-    const loginData = {
-        email,
-        password,
-    };
-
-    const req = {
-        method: "POST",
-        headers: {
-            accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(loginData),
-    };
-
-    const erreur = document.querySelector("#erreur");
-    erreur.innerHTML = "";
-
-    fetch(loginUrl, req)
-        .then((res) => {
-            if (res.status === 200) {
-                return res.json();
-            } else if (res.status === 401) {
-                throw new Error("Mauvais mot de passe");
-            } else {
-                throw new Error("Utilisateur inconnu");
-            }
-        })
-        .then((data) => {
-            localStorage.setItem("token", data.token);
-            window.location.href = "../../index-edit.html";
-        })
-        .catch((error) => {
-            erreur.setAttribute("id", "erreur");
-            erreur.innerHTML = error.message;
-            this.appendChild(erreur);
-        });
-}
 
 loginForm.addEventListener("submit", handleFormSubmit);
