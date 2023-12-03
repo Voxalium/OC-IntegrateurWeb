26863f5b5f651d01c33a5df41786197a55e63067
Voxalium <voxalium@gmail.com>
2023-12-03 12:30:04 +0100

    p8 - Add routing accueil apropos & 404

--- Projet-8/kasa/src/index.js
+++ Projet-8/kasa/src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./styles/index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Apropos from "./pages/Apropros";
+
+//Component
+import Header from "./components/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="*" element={<Error />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/Apropos" element={<Apropos />} />
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
