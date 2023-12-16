7273df6f9c4aa8ceb410fa1b5dbd7f336e3f2282
Voxalium <voxalium@gmail.com>
2023-12-16 15:38:19 +0100

    add Info and Tag components | scss work on Logement

--- Projet-8/kasa/src/components/Dropdown.jsx
+++ Projet-8/kasa/src/components/Dropdown.jsx
@@ -8,16 +8,14 @@ function Dropdown(props) {
     const [isOpen, setIsOpen] = useState(false);
 
     const listItems = items.map((item) => (
-        <li key={item.toString()}>
-            <a href="#">{item} </a>{" "}
-        </li>
+        <li key={item.toString()}>{item}</li>
     ));
     return (
         <article className="dropdown">
             <h3 onClick={() => setIsOpen((prev) => !prev)}>
                 {title} <Arrow isOpen={isOpen} />
             </h3>
-            {isOpen && <ul>{listItems}</ul>}
+            {isOpen && <ul>{listItems} </ul>}
         </article>
     );
 }
--- Projet-8/kasa/src/components/Slideshow.jsx
+++ Projet-8/kasa/src/components/Slideshow.jsx
@@ -1,5 +1,24 @@
-function Slideshow() {
-    return <div className="slideshow"></div>;
+import arrowLeft from "../assets/arrow-left.svg";
+import arrowRight from "../assets/arrow-right.svg";
+
+function Slideshow(props) {
+    const img = props.pictures;
+    return (
+        <div className="slideshow">
+            <img className="img-logement" src="" alt="image logement" />
+            <img
+                className="slideshow-arrow left-arrow"
+                src={arrowLeft}
+                alt="left arrow"
+            />
+            <img
+                className="slideshow-arrow right-arrow"
+                src={arrowRight}
+                alt="right arrow"
+            />
+            <span className="slideshow-numbers"></span>
+        </div>
+    );
 }
 
 export default Slideshow;
