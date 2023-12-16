808c2d3228ed490a1ef7bb36024ac576db2f530e
Voxalium <voxalium@gmail.com>
2023-12-16 15:37:37 +0100

    add Info and Tag components | scss work on Logement

--- Projet-8/kasa/src/pages/Logements/index.jsx
+++ Projet-8/kasa/src/pages/Logements/index.jsx
@@ -1,14 +1,25 @@
 import Dropdown from "../../components/Dropdown";
+import Slideshow from "../../components/Slideshow";
+import Info from "../../components/Info";
+import Tag from "../../components/Tag";
+import data from "../../datas/logements.json";
 
 function Logements() {
     return (
         <div className="logements">
-            <h3>Title</h3>
-            <p>Ville</p>
-            <p>tags</p>
+            <Slideshow />
 
-            <Dropdown title="Description" items={[""]} />
-            <Dropdown title="Equipements" items={[""]} />
+            <Info title={data[0].title} location={data[0].location} />
+
+            <section className="logements-tags">
+                {data[0].tags.map((tag) => (
+                    <Tag key={data[0].id.toString()} tag={tag} />
+                ))}
+            </section>
+            <section className="logements-dropdown">
+                <Dropdown title="Description" items={[data[0].description]} />
+                <Dropdown title="Equipements" items={data[0].equipments} />
+            </section>
         </div>
     );
 }
