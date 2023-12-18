07170f62480aa9a53d2a5af48cc0431095b6bb05
Voxalium <voxalium@gmail.com>
2023-12-18 12:25:59 +0100

    add Profile component

--- Projet-8/kasa/src/pages/Logements/index.jsx
+++ Projet-8/kasa/src/pages/Logements/index.jsx
@@ -2,6 +2,7 @@ import Dropdown from "../../components/Dropdown";
 import Slideshow from "../../components/Slideshow";
 import Info from "../../components/Info";
 import Tag from "../../components/Tag";
+import Profile from "../../components/Profile";
 import { useLoaderData } from "react-router-dom";
 
 function Logements() {
@@ -10,13 +11,22 @@ function Logements() {
         <div className="logements">
             <Slideshow />
 
-            <Info title={data.title} location={data.location} />
-            <section className="logements-tags">
-                {data.tags.map((tag) => (
-                    <Tag key={tag} tag={tag} />
-                ))}
-            </section>
-
+            <div className="logements-info">
+                <div className="logements-description">
+                    <Info title={data.title} location={data.location} />
+                    <section className="logements-tags">
+                        {data.tags.map((tag) => (
+                            <Tag key={tag} tag={tag} />
+                        ))}
+                    </section>
+                </div>
+                <div className="logements-host">
+                    <Profile
+                        name={data.host.name}
+                        picture={data.host.picture}
+                    />
+                </div>
+            </div>
             <section className="logements-dropdown">
                 <Dropdown title="Description" items={[data.description]} />
                 <Dropdown title="Équipements" items={data.equipments} />
