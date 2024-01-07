9fb70db6333dd474dcbe414620b6908ca8d56770
Voxalium <voxalium@gmail.com>
2024-01-07 17:08:21 +0100

    p10 - fix slider & footer

--- Projet-10/Debuggez-une-application-React.JS/src/containers/Events/index.js
+++ Projet-10/Debuggez-une-application-React.JS/src/containers/Events/index.js
@@ -62,7 +62,7 @@ const EventList = () => {
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+              <a key={n+1} href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
               </a>
             ))}
