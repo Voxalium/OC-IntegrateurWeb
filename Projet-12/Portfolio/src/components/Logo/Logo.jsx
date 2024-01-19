59b7ad0fbce4ff2753892c9ac12a1f788516ec34
Voxalium <voxalium@gmail.com>
2024-01-19 14:04:55 +0100

    p10 - branch

--- Projet-12/Portfolio/src/components/Logo/Logo.jsx
+++
@@ -1,5 +0,0 @@
-import logo from "../../assets/logo.png";
-
-export const Logo = () => {
-    return <img src={logo} alt="Logo de site" className="logo" />;
-};
--- Projet-8/kasa/src/components/Logement/Tag.jsx
+++
@@ -1,5 +0,0 @@
-function Tag({ tag }) {
-    return <span className="tag">{tag}</span>;
-}
-
-export default Tag;
