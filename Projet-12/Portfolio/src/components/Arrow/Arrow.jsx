0ff52b4411f47786fd836a0ab406c3cfe6181806
Voxalium <voxalium@gmail.com>
2024-03-13 11:46:24 +0100

    P12-Change icon home

--- Projet-12/Portfolio/src/components/Arrow/Arrow.jsx
+++ Projet-12/Portfolio/src/components/Arrow/Arrow.jsx
@@ -1,12 +1,23 @@
 import arrowUp from "/ui/arrow_up.webp";
 import arrowDown from "/ui/arrow_down.webp";
+import arrowHome from "/ui/arrow_home.webp";
 export const Arrow = ({ link, direction }) => {
-    let img = "";
-    direction == "down" ? (img = arrowDown) : (img = arrowUp);
+  let img = "";
+  direction == "down" ? (img = arrowDown) : (img = arrowUp);
+  switch (direction) {
+    case "down":
+      img = arrowDown;
+      break;
+    case "up":
+      img = arrowUp;
+      break;
+    case "home":
+      img = arrowHome;
+  }
 
-    return (
-        <a href={link} className={`arrow ${direction}`}>
-            <img src={img} alt="navigation arrow" />
-        </a>
-    );
+  return (
+    <a href={link} className={`arrow ${direction}`}>
+      <img src={img} alt="navigation arrow" />
+    </a>
+  );
 };
--- Projet-12/Portfolio/src/views/Skills/Skills.jsx
+++ Projet-12/Portfolio/src/views/Skills/Skills.jsx
@@ -3,36 +3,36 @@ import { CardOs } from "../../components/Card/CardOs";
 import { Card } from "../../components/Card/Card";
 import { skills } from "../../datas/data.json";
 export const Skills = () => {
-    return (
-        <section id="skills" className="skills">
-            <Arrow link="#project" direction="up" />
-            <h2>Mes compétences</h2>
+  return (
+    <section id="skills" className="skills">
+      <Arrow link="#project" direction="up" />
+      <h2>Mes compétences</h2>
 
-            <h3>Web</h3>
-            <div className="cards">
-                <Card skill={filterData("Code", "web")} />
-            </div>
+      <h3>Web</h3>
+      <div className="cards">
+        <Card skill={filterData("Code", "web")} />
+      </div>
 
-            <h3>Systèmes d'exploitation</h3>
+      <h3>Systèmes d'exploitation</h3>
 
-            <div className="system">
-                {filterData("Softwares", "os").map((skill) => (
-                    <CardOs
-                        key={`image ${skill.name}`}
-                        title={skill.name}
-                        content={skill.content}
-                        image={skill.logo}
-                    />
-                ))}
-            </div>
+      <div className="system">
+        {filterData("Softwares", "os").map((skill) => (
+          <CardOs
+            key={`image ${skill.name}`}
+            title={skill.name}
+            content={skill.content}
+            image={skill.logo}
+          />
+        ))}
+      </div>
 
-            <Arrow link="#intro" direction="down" />
-        </section>
-    );
+      <Arrow link="#intro" direction="home" />
+    </section>
+  );
 };
 
 const filterData = (title, category) => {
-    return skills
-        .find((f) => f.title === title)
-        .skill.filter((s) => s.category === category);
+  return skills
+    .find((f) => f.title === title)
+    .skill.filter((s) => s.category === category);
 };
